76a04eee4d8079631398500181b55fe538dd31ed
theskript <theskript@gmail.com>
2014-01-29 21:00:18 -0500

    Update rsr.php

--- rsr.php
+++ rsr.php
@@ -2,22 +2,25 @@
 /****************************************************************************************
  * 
  * 
- * 	Send a svg file to readysetraphael.com using curl and get raphaeljs code.
+ *         Send a svg file to readysetraphael.com using curl and get raphaeljs code.
  * 
  * 
 ****************************************************************************************/
 
 
 $file = $_GET['url'];
+if (!file_exists('_rsr')) {
+    mkdir('_rsr', 0777, true);
+}
 $tmpfile = uniqid(rand(), true) . '.svg';
 file_put_contents('_rsr/'.$tmpfile, fopen('http://'.$file.'', 'r'));
 $filepath = realpath('./_rsr/'.$tmpfile);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'http://www.readysetraphael.com/submit.php');
 $post_data = array(
-	'MAX_FILE_SIZE' => '100000000', 
-	'image' => '@'.$filepath,
-	'submit' => 'Send'
+        'MAX_FILE_SIZE' => '100000000', 
+        'image' => '@'.$filepath,
+        'submit' => 'Send'
 );
 
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -30,7 +33,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.3) Gecko/20070309 Firefox/2.0.0.3");
 $html = curl_exec($ch);
 if (preg_match("/<div id=\"output\">([^`]*?)<\/div>/", $html, $match)) {
-	$data = ltrim(rtrim(trim($match[1])));
+        $data = ltrim(rtrim(trim($match[1])));
 }
 $file = array_reverse(explode('/',$file));
 $file = substr($file[0],0,-4);
